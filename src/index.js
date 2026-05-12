b0f7145a4df2683e43eecffb3182838ff59dad8d
mouaddev24 <mouadmouad659@gmail.com>
2026-05-12 21:20:53 +0200

    Add src/index.js

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const { Client, GatewayIntentBits, Collection, Partials } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const { initDB } = require('./utils/database');
+const { seedDefaultData } = require('./data/seed');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
+
+client.commands = new Collection();
+client.panels = new Collection();
+
+// Load commands
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  if (command.data && command.execute) {
+    client.commands.set(command.data.name, command);
+  }
+}
+
+// Load events
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(f => f.endsWith('.js'));
+for (const file of eventFiles) {
+  const event = require(path.join(eventsPath, file));
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+// Init DB and seed, then login
+initDB();
+seedDefaultData();
+
+client.login(process.env.DISCORD_TOKEN);
